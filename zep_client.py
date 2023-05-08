720676feb1fca674885c55ab898914bc9272e7ab
Sharath Rajasekar <sharathr@gmail.com>
2023-05-08 10:35:55 -0700

    Update zep_client.py
    Sphinx compatible doc

--- zep_client.py
+++ zep_client.py
@@ -5,7 +5,29 @@ from typing import Optional, List
 from utils import _sync
 
 class ZepClient:
-    # ZepClient class implementation
+    """
+    ZepClient class implementation.
+
+    Attributes
+    ----------
+    base_url : str
+        The base URL of the API.
+    client : httpx.AsyncClient
+        The HTTP client used for making API requests.
+
+    Methods
+    -------
+    get_memory(session_id: str, lastn: Optional[int] = None) -> List[Memory]:
+        Retrieve memory for the specified session.
+    add_memory(session_id: str, memory_messages: Memory) -> str:
+        Add memory to the specified session.
+    delete_memory(session_id: str) -> str:
+        Delete memory for the specified session.
+    search_memory(session_id: str, search_payload: SearchPayload, limit: Optional[int] = None) -> List[SearchResult]:
+        Search memory for the specified session.
+    close() -> None:
+        Close the HTTP client.
+    """
 
     # Asynchronous context manager entry point
     async def __aenter__(self) -> 'ZepClient':
@@ -19,8 +41,10 @@ class ZepClient:
         """
         Initialize the ZepClient with the specified base URL.
 
-        Parameters:
-            base_url (str): The base URL of the API.
+        Parameters
+        ----------
+        base_url : str
+            The base URL of the API.
         """
         self.base_url = base_url
         self.client = httpx.AsyncClient()
@@ -35,21 +59,48 @@ class ZepClient:
 
     @_sync
     def get_memory(self, session_id: str, lastn: Optional[int] = None) -> List[Memory]:
+        """
+        Retrieve memory for the specified session. This method is a synchronous wrapper for the asynchronous method `aget_memory`.
+
+        Parameters
+        ----------
+        session_id : str
+            The ID of the session for which to retrieve memory.
+        lastn : Optional[int], optional
+            The number of most recent memory entries to retrieve. Defaults to None (all entries).
+
+        Returns
+        -------
+        List[Memory]
+            A list of Memory objects representing the retrieved memory entries.
+
+        Raises
+        ------
+        UnexpectedResponseError
+            If the API response format is unexpected.
+        """
         return self.aget_memory(session_id, lastn)
 
     async def aget_memory(self, session_id: str, lastn: Optional[int] = None) -> List[Memory]:
         """
         Asynchronously retrieve memory for the specified session.
 
-        Parameters:
-            session_id (str): The ID of the session for which to retrieve memory.
-            lastn (Optional[int]): The number of most recent memory entries to retrieve. Defaults to None (all entries).
+        Parameters
+        ----------
+        session_id : str
+            The ID of the session for which to retrieve memory.
+        lastn : Optional[int], optional
+            The number of most recent memory entries to retrieve. Defaults to None (all entries).
 
-        Returns:
-            List[Memory]: A list of Memory objects representing the retrieved memory entries.
+        Returns
+        -------
+        List[Memory]
+            A list of Memory objects representing the retrieved memory entries.
 
-        Raises:
-            UnexpectedResponseError: If the API response format is unexpected.
+        Raises
+        ------
+        UnexpectedResponseError
+            If the API response format is unexpected.
         """
         url = f"{self.base_url}/api/v1/sessions/{session_id}/memory"
         params = {'lastn': lastn} if lastn is not None else {}  # Include 'lastn' as a query parameter if provided
@@ -77,21 +128,48 @@ class ZepClient:
 
     @_sync
     def add_memory(self, session_id: str, memory_messages: Memory) -> str:
+        """
+        Add memory to the specified session. This method is a synchronous wrapper for the asynchronous method `aadd_memory`.
+
+        Parameters
+        ----------
+        session_id : str
+            The ID of the session to which memory should be added.
+        memory_messages : Memory
+            A Memory object representing the memory messages to be added.
+
+        Returns
+        -------
+        str
+            The response text from the API.
+
+        Raises
+        ------
+        UnexpectedResponseError
+            If the API response format is unexpected.
+        """
         return self.aadd_memory(session_id, memory_messages)
 
     async def aadd_memory(self, session_id: str, memory_messages: Memory) -> str:
         """
         Asynchronously add memory to the specified session.
 
-        Parameters:
-            session_id (str): The ID of the session to which memory should be added.
-            memory_messages (Memory): A Memory object representing the memory messages to be added.
+        Parameters
+        ----------
+        session_id : str
+            The ID of the session to which memory should be added.
+        memory_messages : Memory
+            A Memory object representing the memory messages to be added.
 
-        Returns:
-            str: The response text from the API.
+        Returns
+        -------
+        str
+            The response text from the API.
 
-        Raises:
-            UnexpectedResponseError: If the API response format is unexpected.
+        Raises
+        ------
+        UnexpectedResponseError
+            If the API response format is unexpected.
         """
         response = await self.client.post(
             f'{self.base_url}/api/v1/sessions/{session_id}/memory',
@@ -104,20 +182,44 @@ class ZepClient:
 
     @_sync
     def delete_memory(self, session_id: str) -> str:
+        """
+        Delete memory for the specified session. This method is a synchronous wrapper for the asynchronous method `adelete_memory`.
+
+        Parameters
+        ----------
+        session_id : str
+            The ID of the session for which memory should be deleted.
+
+        Returns
+        -------
+        str
+            The response text from the API.
+
+        Raises
+        ------
+        UnexpectedResponseError
+            If the API response format is unexpected.
+        """
         return self.adelete_memory(session_id)
 
     async def adelete_memory(self, session_id: str) -> str:
         """
         Asynchronously delete memory for the specified session.
 
-        Parameters:
-            session_id (str): The ID of the session for which memory should be deleted.
+        Parameters
+        ----------
+        session_id : str
+            The ID of the session for which memory should be deleted.
 
-        Returns:
-            str: The response text from the API.
+        Returns
+        -------
+        str
+            The response text from the API.
 
-        Raises:
-            UnexpectedResponseError: If the API response format is unexpected.
+        Raises
+        ------
+        UnexpectedResponseError
+            If the API response format is unexpected.
         """
         response = await self.client.delete(f'{self.base_url}/api/v1/sessions/{session_id}/memory')
         if response.status_code != 200:
@@ -126,22 +228,52 @@ class ZepClient:
 
     @_sync
     def search_memory(self, session_id: str, search_payload: SearchPayload, limit: Optional[int] = None) -> List[SearchResult]:
+        """
+        Search memory for the specified session. This method is a synchronous wrapper for the asynchronous method `asearch_memory`.
+
+        Parameters
+        ----------
+        session_id : str
+            The ID of the session for which memory should be searched.
+        search_payload : SearchPayload
+            A SearchPayload object representing the search query.
+        limit : Optional[int], optional
+            The maximum number of search results to return. Defaults to None (no limit).
+
+        Returns
+        -------
+        List[SearchResult]
+            A list of SearchResult objects representing the search results.
+
+        Raises
+        ------
+        UnexpectedResponseError
+            If the API response format is unexpected.
+        """
         return self.asearch_memory(session_id, search_payload, limit)
 
     async def asearch_memory(self, session_id: str, search_payload: SearchPayload, limit: Optional[int] = None) -> List[SearchResult]:
         """
         Asynchronously search memory for the specified session.
 
-        Parameters:
-            session_id (str): The ID of the session for which memory should be searched.
-            search_payload (SearchPayload): A SearchPayload object representing the search query.
-            limit (Optional[int]): The maximum number of search results to return. Defaults to None (no limit).
+        Parameters
+        ----------
+        session_id : str
+            The ID of the session for which memory should be searched.
+        search_payload : SearchPayload
+            A SearchPayload object representing the search query.
+        limit : Optional[int], optional
+            The maximum number of search results to return. Defaults to None (no limit).
 
-        Returns:
-            List[SearchResult]: A list of SearchResult objects representing the search results.
+        Returns
+        -------
+        List[SearchResult]
+            A list of SearchResult objects representing the search results.
 
-        Raises:
-            UnexpectedResponseError: If the API response format is unexpected.
+        Raises
+        ------
+        UnexpectedResponseError
+            If the API response format is unexpected.
         """
         params = {'limit': limit} if limit is not None else {}
         response = await self.client.post(f'{self.base_url}/api/v1/sessions/{session_id}/search', json=search_payload.__dict__, params=params)
@@ -153,6 +285,6 @@ class ZepClient:
         """
         Asynchronously close the HTTP client.
 
-        This method should be called when the ZepClient is no longer needed to release resources.
+        [Optional] This method may be called when the ZepClient is no longer needed to release resources.
         """
         await self.client.aclose()
