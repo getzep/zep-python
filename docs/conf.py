3b32a043fea88cadec835bf0ea4e1c6df53d281d
Daniel Chalef <daniel.chalef@private.org>
2023-05-09 07:45:07 -0700

    fix extensions

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,11 @@ author = "Sharath Rajasekar, Daniel Chalef"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["autodoc", "viewcode", "sphinxawesome_theme"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinxawesome_theme",
+]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
