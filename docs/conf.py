e7fb98799077a39279c0644c138ef88812c642ec
Daniel Chalef <daniel.chalef@private.org>
2023-05-09 07:19:02 -0700

    sphix setup

--- docs/conf.py
+++ docs/conf.py
@@ -6,22 +6,21 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'zep-python'
-copyright = '2023, Sharath Rajasekar, Daniel Chalef'
-author = 'Sharath Rajasekar, Daniel Chalef'
+project = "zep-python"
+copyright = "2023, Sharath Rajasekar, Daniel Chalef"
+author = "Sharath Rajasekar, Daniel Chalef"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["autodoc", "viewcode", "sphinxawesome_theme"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinxawesome_theme"
+html_static_path = ["_static"]
