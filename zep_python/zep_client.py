0b887470ebc3ec6687cee58d4948285ab20d1a64
Daniel Chalef <131175+danielchalef@users.noreply.github.com>
2023-05-25 07:53:07 -0700

    refactor to remove async loop wrapper around sync api (#40)

--- zep_python/zep_client.py
+++ zep_python/zep_client.py
@@ -1,16 +1,14 @@
 from __future__ import annotations
 
-import asyncio
 from types import TracebackType
-from typing import List, Optional, Type
+from typing import Any, Dict, List, Optional, Type
 
 import httpx
 
 from zep_python.exceptions import APIError, NotFoundError
 from zep_python.models import Memory, Message, SearchPayload, SearchResult, Summary
-from zep_python.utils import sync
 
-API_BASEURL = "/api/v1"
+API_BASE_PATH = "/api/v1"
 
 
 class ZepClient:
@@ -48,40 +46,70 @@ class ZepClient:
         base_url : str
             The base URL of the API.
         """
-        self.base_url = base_url
-        self.client = httpx.AsyncClient()
+        self.base_url = f"{base_url}{API_BASE_PATH}"
+        self.aclient = httpx.AsyncClient(base_url=self.base_url)
+        self.client = httpx.Client(base_url=self.base_url)
 
-    # Asynchronous context manager entry point
     async def __aenter__(self) -> "ZepClient":
+        """Asynchronous context manager entry point"""
         return self
 
-    # Sync context manager exit point
     async def __aexit__(
         self,
         exc_type: Type[Exception],
         exc_val: Exception,
         exc_tb: TracebackType,
     ) -> None:
+        """Asynchronous context manager exit point"""
         await self.aclose()
 
-    # Asynchronous context manager entry point
     def __enter__(self) -> "ZepClient":
+        """Sync context manager entry point"""
         return self
 
-    # Sync context manager exit point
     def __exit__(
         self,
         exc_type: Type[Exception],
         exc_val: Exception,
         exc_tb: TracebackType,
     ) -> None:
+        """Sync context manager exit point"""
         self.close()
 
-    @sync
+    def _parse_get_memory_response(self, response_data: Any) -> Memory:
+        """Parse the response from the get_memory API call."""
+        messages: List[Message]
+        try:
+            messages = [
+                Message.parse_obj(m) for m in response_data.get("messages", None)
+            ]
+            if len(messages) == 0:
+                raise ValueError("Messages can't be empty")
+        except (TypeError, ValueError) as e:
+            raise APIError("Unexpected response format from the API") from e
+
+        summary: Optional[Summary] = None
+        if response_data.get("summary", None) is not None:
+            summary = Summary.parse_obj(response_data["summary"])
+
+        memory = Memory(
+            messages=messages,
+            # Add the 'summary' field if it is present in the response.
+            summary=summary,
+            # Add any other fields from the response that are relevant to the
+            # Memory class.
+        )
+        return memory
+
+    def _gen_get_params(self, lastn: Optional[int] = None) -> Dict[str, Any]:
+        params = {}
+        if lastn is not None:
+            params["lastn"] = lastn
+        return params
+
     def get_memory(self, session_id: str, lastn: Optional[int] = None) -> Memory:
         """
-        Retrieve memory for the specified session. This method is a synchronous wrapper
-        for the asynchronous method `aget_memory`.
+        Retrieve memory for the specified session.
 
         Parameters
         ----------
@@ -101,12 +129,23 @@ class ZepClient:
         APIError
             If the API response format is unexpected.
         """
-        # we've wrapped the function in a decorator that will run it synchronously.
-        # ignore the type error.
+
         if session_id is None or session_id == "":
             raise ValueError("session_id must be provided")
 
-        return self.aget_memory(session_id, lastn)  # type: ignore
+        url = f"/sessions/{session_id}/memory"
+        params = self._gen_get_params(lastn)
+        response = self.client.get(url, params=params)
+
+        if response.status_code == 404:
+            raise NotFoundError(f"No memory found for session {session_id}")
+
+        if response.status_code != 200:
+            raise APIError(f"Unexpected status code: {response.status_code}")
+
+        response_data = response.json()
+
+        return self._parse_get_memory_response(response_data)
 
     async def aget_memory(self, session_id: str, lastn: Optional[int] = None) -> Memory:
         """
@@ -133,11 +172,9 @@ class ZepClient:
         if session_id is None or session_id == "":
             raise ValueError("session_id must be provided")
 
-        url = f"{self.base_url}{API_BASEURL}/sessions/{session_id}/memory"
-        params = (
-            {"lastn": lastn} if lastn is not None else {}
-        )  # Include 'lastn' as a query parameter if provided
-        response = await self.client.get(url, params=params)
+        url = f"/sessions/{session_id}/memory"
+        params = self._gen_get_params(lastn)
+        response = await self.aclient.get(url, params=params)
 
         if response.status_code == 404:
             raise NotFoundError(f"No memory found for session {session_id}")
@@ -146,34 +183,12 @@ class ZepClient:
             raise APIError(f"Unexpected status code: {response.status_code}")
 
         response_data = response.json()
-        messages: List[Message]
-        try:
-            messages = [
-                Message.parse_obj(m) for m in response_data.get("messages", None)
-            ]
-            if len(messages) == 0:
-                raise ValueError("Messages can't be empty")
-        except (TypeError, ValueError) as e:
-            raise APIError("Unexpected response format from the API") from e
 
-        summary: Optional[Summary] = None
-        if response_data.get("summary", None) is not None:
-            summary = Summary.parse_obj(response_data["summary"])
+        return self._parse_get_memory_response(response_data)
 
-        memory = Memory(
-            messages=messages,
-            # Add the 'summary' field if it is present in the response.
-            summary=summary,
-            # Add any other fields from the response that are relevant to the
-            # Memory class.
-        )
-        return memory
-
-    @sync
     def add_memory(self, session_id: str, memory_messages: Memory) -> str:
         """
-        Add memory to the specified session. This method is a synchronous wrapper for
-        the asynchronous method `aadd_memory`.
+        Add memory to the specified session.
 
         Parameters
         ----------
@@ -192,12 +207,17 @@ class ZepClient:
         APIError
             If the API response format is unexpected.
         """
-        # we've wrapped the function in a decorator that will run it synchronously.
-        # ignore the type error.
         if session_id is None or session_id == "":
             raise ValueError("session_id must be provided")
 
-        return self.aadd_memory(session_id, memory_messages)  # type: ignore
+        response = self.client.post(
+            f"/sessions/{session_id}/memory",
+            json=memory_messages.dict(exclude_none=True),
+        )
+        if response.status_code != 200:
+            raise APIError(f"Unexpected status code: {response.status_code}")
+
+        return response.text
 
     async def aadd_memory(self, session_id: str, memory_messages: Memory) -> str:
         """
@@ -223,8 +243,8 @@ class ZepClient:
         if session_id is None or session_id == "":
             raise ValueError("session_id must be provided")
 
-        response = await self.client.post(
-            f"{self.base_url}{API_BASEURL}/sessions/{session_id}/memory",
+        response = await self.aclient.post(
+            f"/sessions/{session_id}/memory",
             json=memory_messages.dict(exclude_none=True),
         )
         if response.status_code != 200:
@@ -232,11 +252,9 @@ class ZepClient:
 
         return response.text
 
-    @sync
     def delete_memory(self, session_id: str) -> str:
         """
-        Delete memory for the specified session. This method is a synchronous wrapper
-        for the asynchronous method `adelete_memory`.
+        Delete memory for the specified session.
 
         Parameters
         ----------
@@ -253,12 +271,16 @@ class ZepClient:
         APIError
             If the API response format is unexpected.
         """
-        # we've wrapped the function in a decorator that will run it synchronously.
-        # ignore the type error.
         if session_id is None or session_id == "":
             raise ValueError("session_id must be provided")
 
-        return self.adelete_memory(session_id)  # type: ignore
+        response = self.client.delete(f"/sessions/{session_id}/memory")
+        if response.status_code == 404:
+            raise NotFoundError(f"No session found for session {session_id}")
+
+        if response.status_code != 200:
+            raise APIError(f"Unexpected status code: {response.status_code}")
+        return response.text
 
     async def adelete_memory(self, session_id: str) -> str:
         """
@@ -282,9 +304,7 @@ class ZepClient:
         if session_id is None or session_id == "":
             raise ValueError("session_id must be provided")
 
-        response = await self.client.delete(
-            f"{self.base_url}{API_BASEURL}/sessions/{session_id}/memory"
-        )
+        response = await self.aclient.delete(f"/sessions/{session_id}/memory")
         if response.status_code == 404:
             raise NotFoundError(f"No session found for session {session_id}")
 
@@ -292,7 +312,6 @@ class ZepClient:
             raise APIError(f"Unexpected status code: {response.status_code}")
         return response.text
 
-    @sync
     def search_memory(
         self,
         session_id: str,
@@ -300,8 +319,7 @@ class ZepClient:
         limit: Optional[int] = None,
     ) -> List[SearchResult]:
         """
-        Search memory for the specified session. This method is a synchronous wrapper
-        for the asynchronous method `asearch_memory`.
+        Search memory for the specified session.
 
         Parameters
         ----------
@@ -322,12 +340,23 @@ class ZepClient:
         APIError
             If the API response format is unexpected.
         """
-        # we've wrapped the function in a decorator that will run it synchronously.
-        # ignore the type error.
         if session_id is None or session_id == "":
             raise ValueError("session_id must be provided")
 
-        return self.asearch_memory(session_id, search_payload, limit)  # type: ignore
+        if search_payload is None:
+            raise ValueError("search_payload must be provided")
+
+        params = {"limit": limit} if limit is not None else {}
+        response = self.client.post(
+            f"/sessions/{session_id}/search",
+            json=search_payload.dict(),
+            params=params,
+        )
+        if response.status_code == 404:
+            raise NotFoundError("No query results found")
+        if response.status_code != 200:
+            raise APIError(f"Unexpected status code: {response.status_code}")
+        return [SearchResult(**search_result) for search_result in response.json()]
 
     async def asearch_memory(
         self,
@@ -364,8 +393,8 @@ class ZepClient:
             raise ValueError("search_payload must be provided")
 
         params = {"limit": limit} if limit is not None else {}
-        response = await self.client.post(
-            f"{self.base_url}{API_BASEURL}/sessions/{session_id}/search",
+        response = await self.aclient.post(
+            f"/sessions/{session_id}/search",
             json=search_payload.dict(),
             params=params,
         )
@@ -382,7 +411,7 @@ class ZepClient:
         [Optional] This method may be called when the ZepClient is no longer needed to
         release resources.
         """
-        await self.client.aclose()
+        await self.aclient.aclose()
 
     def close(self) -> None:
         """
@@ -391,4 +420,4 @@ class ZepClient:
         [Optional] This method may be called when the ZepClient is no longer needed to
         release resources.
         """
-        asyncio.run(self.client.aclose())
+        self.client.close()
