6dad65780cba7cc81c277005ee4ffeb4b4d5e468
Daniel Chalef <131175+danielchalef@users.noreply.github.com>
2023-08-26 10:48:08 -0700

    fix/Deprecate warning for experimental LangChain VectorStore; other bugfixes (#73)
    * LangChain ZepVectorStore deprecation notice
    
    * strip trailing / from base_url

--- zep_python/zep_client.py
+++ zep_python/zep_client.py
@@ -250,6 +250,7 @@ def concat_url(base_url: str, path: str) -> str:
     str
         The joined URL.
     """
+    base_url = base_url.rstrip("/")
     return urljoin(base_url + "/", path.lstrip("/"))
 
 
