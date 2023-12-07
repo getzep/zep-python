242231da14e6e9c00e4f4353de7ae0aa78606755
Peter <pb@Peters-MacBook-Pro.local>
2023-12-06 18:13:26 -0700

    Messages example cleaned up

--- examples/messages.py
+++ examples/messages.py
@@ -16,84 +16,100 @@ from zep_python.memory import Session
 from zep_python.message import Message
 from zep_python.user import CreateUserRequest
 
-def main() -> None:
-    base_url = "http://localhost:8000"  # TODO: Replace with Zep API URL
-    api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.e30.nEJCptN3CRfD_SQ4om4Oa2yh-ARPI41qkjj0La0Hw54"  # TODO: Replace with your API key
-    with ZepClient(base_url, api_key) as client:
-        # Example usage
-        # Create a user
-        user_id = uuid.uuid4().hex
-        user_request = CreateUserRequest(
-            user_id=user_id,
-            email="user@example.com",
-            first_name="John",
-            last_name="Doe",
-            metadata={"foo": "bar"},
+def create_user(client):
+    user_id = uuid.uuid4().hex
+    user_request = CreateUserRequest(
+        user_id=user_id,
+        email="user@example.com",
+        first_name="John",
+        last_name="Doe",
+        metadata={"foo": "bar"},
+    )
+    try:
+        user = client.user.add(user_request)
+        print(f"Created user: {user.user_id}")
+        return user
+    except APIError as e:
+        print(f"Failed to create user: {e}")
+
+def create_session(client, user_id):
+    session_id = uuid.uuid4().hex
+    print(f"Creating session: {session_id}")
+    try:
+        session = Session(
+            session_id=session_id, user_id=user_id, metadata={"foo": "bar"}
         )
-        try:
-            user = client.user.add(user_request)
-            print(f"Created user: {user.user_id}")
-        except APIError as e:
-            print(f"Failed to create user: {e}")
+        result = client.memory.add_session(session)
+        print(f"Session created: {result}")
+        return session_id
+    except APIError as e:
+        print(f"Unable to create session {session_id}. Error: {e}")
 
-        # Create session associated with the above user
-        session_id = uuid.uuid4().hex
-        print(f"Creating session: {session_id}")
-        try:
-            session = Session(
-                session_id=session_id, user_id=user_id, metadata={"foo": "bar"}
-            )
-            result = client.memory.add_session(session)
-            print(f"Session created: {result}")
-        except APIError as e:
-            print(f"Unable to create session {session_id}. Error: {e}")
+def add_memory_to_session(client, session_id, history):
+    print(f"addMemory for Session: {session_id}")
+    try:
+        for m in history:
+            message = Message(**m)
+            memory = Memory(messages=[message])
+            client.memory.add_memory(session_id, memory)
+        print(f"Added {len(history)} messages to memory for session {session_id}")
+    except NotFoundError as e:
+        print(f"Memory not found for session {session_id}. Got error: {e}")
+    except APIError as e:
+        print(f"API error occurred while adding memory to session {session_id}. Got error: {e}")
 
-        print(f"\n2---addMemory for Session: {session_id}")
-        try:
-            for m in history:
-                message = Message(**m)
-                memory = Memory(messages=[message])
-                client.memory.add_memory(session_id, memory)
-            print(f"Added {len(history)} messages to memory for session {session_id}")
-        except NotFoundError as e:
-            print(f"Memory not found for session {session_id}. Got error: {e}")
-        except APIError as e:
-            print(
-                f"API error occurred while adding memory to session {session_id}. Got"
-                f" error: {e}"
-            )
-
-        # Get session messages
+def get_and_print_session_messages(client, session_id):
+    try:
         session_messages = client.message.get_session_messages(session_id)
         print(f"Get Session Messages: {session_messages}")
+        return session_messages
+    except NotFoundError as e:
+        print(f"Session not found for Session {session_id}. Got error: {e}")
+    except APIError as e:
+        print(f"API error occurred while getting messages for Session {session_id}. Got error: {e}")
 
-        # Get Session Message
-        
-        # get first session message
-        first_session_message_id = session_messages[0].uuid
-        print(f"Get Session Message: {first_session_message_id}")
-
-        session_message = client.message.get_session_message(session_id, first_session_message_id)
+def get_and_print_first_session_message(client, session_id, message_id):
+    try:
+        session_message = client.message.get_session_message(session_id, message_id)
         print(f"Get Session Message: {session_message}")
+    except NotFoundError as e:
+        print(f"Session Message not found for Session {session_id} and Message {message_id}. Got error: {e}")
+    except APIError as e:    
+        print(f"API error occurred while getting message for Session {session_id} and Message {message_id}. Got error: {e}")
 
-        updated_session_message_metadata = {"metadata": {"foo": "bar"}}
+
+def update_and_print_session_message_metadata(client, session_id, first_session_message_id):
+    updated_session_message_metadata = {"metadata": {"foo": "bar"}}
+    try:
         updated_session_message = client.message.update_message_metadata(
             session_id, first_session_message_id, updated_session_message_metadata
         )
         print(f"Updated Session Message Metadata: {updated_session_message}")
+    except NotFoundError:
+        print(f"Session Message not found for Session {session_id} and Message {first_session_message_id}. Got error: {e}")
+    except APIError as e:
+        print(f"API error occurred while updating message metadata for Session {session_id} and Message {first_session_message_id}. Got error: {e}")
 
-        # Delete memory
-        print(f"Deleting memory for Session: {session_id}")
-        try:
-            result = client.memory.delete_memory(session_id)
-            print(f"Memory deleted: {result}")
-        except NotFoundError:
-            print("Memory not found for Session" + session_id)
+def delete_and_print_memory_for_session(client, session_id):
+    print(f"Deleting memory for Session: {session_id}")
+    try:
+        result = client.memory.delete_memory(session_id)
+        print(f"Memory deleted: {result}")
+    except NotFoundError:
+        print("Memory not found for Session" + session_id)
 
-
-
-
-
+def main():
+    base_url = "http://localhost:8000"
+    api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.e30.nEJCptN3CRfD_SQ4om4Oa2yh-ARPI41qkjj0La0Hw54"
+    with ZepClient(base_url, api_key) as client:
+        user = create_user(client)
+        session_id = create_session(client, user.user_id)
+        add_memory_to_session(client, session_id, history)
+        session_messages = get_and_print_session_messages(client, session_id)
+        first_session_message_id = session_messages[0].uuid
+        get_and_print_first_session_message(client, session_id, first_session_message_id)
+        update_and_print_session_message_metadata(client, session_id, first_session_message_id)
+        delete_and_print_memory_for_session(client, session_id)
 
 if __name__ == "__main__":
     main()
