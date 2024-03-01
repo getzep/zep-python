3a7e6ef2e49d46748cb30188786e7ee995bf2bef
paulpaliychuk <pavlo.paliychuk.ca@gmail.com>
2024-03-01 16:38:10 -0500

    chore: Fix linter errors

--- examples/chainlit/app.py
+++ examples/chainlit/app.py
@@ -1,18 +1,20 @@
-from zep_python import ZepClient
 import os
-from dotenv import load_dotenv, find_dotenv
 import uuid
-from zep_python.message import Message
-from zep_python.memory import Memory
+
 import chainlit as cl
-from openai import AsyncOpenAI
 from chainlit.input_widget import Select
+from dotenv import find_dotenv, load_dotenv
+from openai import AsyncOpenAI
+
+from zep_python import ZepClient
+from zep_python.memory import Memory
+from zep_python.message import Message
 
 load_dotenv(dotenv_path=find_dotenv())
 
-API_KEY = os.environ.get('ZEP_API_KEY')
-ZEP_API_URL = os.environ.get('ZEP_API_URL')
-OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+API_KEY = os.environ.get("ZEP_API_KEY")
+ZEP_API_URL = os.environ.get("ZEP_API_URL")
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
 zep = ZepClient(api_key=API_KEY, api_url=ZEP_API_URL)
 
@@ -40,7 +42,7 @@ async def on_chat_start():
 
     cl.user_session.set("id", uuid.uuid4())
     if chat_profile == "Memory + RAG":
-        collection_name = os.environ.get('ZEP_COLLECTION')
+        collection_name = os.environ.get("ZEP_COLLECTION")
         if not collection_name:
             raise ValueError("ZEP_COLLECTION environment variable is not set")
         try:
@@ -49,24 +51,28 @@ async def on_chat_start():
             raise ValueError(f"Error accessing collection {collection_name}: {e}")
 
 
-
 @cl.on_settings_update
 async def update_settings(settings):
     cl.user_session.set("memory_type", settings["memory_type"])
 
+
 @cl.step(name="ZepHistory", type="retrieval")
 async def get_history():
     session_id = cl.user_session.get("id")
-    memory = await zep.memory.aget_memory(session_id, cl.user_session.get("memory_type"))
+    memory = await zep.memory.aget_memory(
+        session_id, cl.user_session.get("memory_type")
+    )
     summary = memory.summary
     message_history = []
     if summary:
         message_history.append({"role": "system", "content": summary.content})
     for message in memory.messages:
-        message_history.append({
-            "role": "assistant" if message.role == "ai" else "user",
-            "content": message.content
-        })
+        message_history.append(
+            {
+                "role": "assistant" if message.role == "ai" else "user",
+                "content": message.content,
+            }
+        )
     return message_history
 
 
@@ -118,11 +124,14 @@ async def call_openai(messages):
 @cl.on_message
 async def on_message(message: cl.Message):
     session_id = cl.user_session.get("id")
-    await zep.memory.aadd_memory(session_id, Memory(
-        messages=[
-            Message(role="user", content=message.content),
-        ]
-    ))
+    await zep.memory.aadd_memory(
+        session_id,
+        Memory(
+            messages=[
+                Message(role="user", content=message.content),
+            ]
+        ),
+    )
 
     msg = cl.Message(author="Assistant", content="")
     await msg.send()
