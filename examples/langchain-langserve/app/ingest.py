37c401e093aeeaff4d7b79c93f194ac7541be05c
paulpaliychuk <pavlo.paliychuk.ca@gmail.com>
2024-04-25 13:28:13 -0400

    chore: Update examples

---
+++ examples/langchain-langserve/app/ingest.py
@@ -0,0 +1,54 @@
+# Ingest Documents into a Zep Collection
+import os
+
+from dotenv import find_dotenv, load_dotenv
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import WebBaseLoader
+
+from zep.client import Zep
+from zep.langchain import ZepVectorStore
+
+load_dotenv(dotenv_path=find_dotenv())
+
+SOURCE = "https://en.wikipedia.org/wiki/Leonard_Bernstein"  # noqa: E501
+
+ZEP_API_URL = os.environ.get(
+    "ZEP_API_URL"
+)  # only required if you're using Zep Open Source
+
+ZEP_API_KEY = os.environ.get("ZEP_API_KEY")  # Required for Zep Cloud
+if ZEP_API_KEY is None:
+    raise ValueError(
+        "ZEP_API_KEY is required for Zep Cloud. "
+        "Remove this check if using Zep Open Source."
+    )
+
+ZEP_COLLECTION_NAME = os.environ.get("ZEP_COLLECTION_NAME")
+if ZEP_COLLECTION_NAME is None:
+    raise ValueError("ZEP_COLLECTION_NAME is required for ingestion. ")
+
+zep = Zep(
+    api_key=ZEP_API_KEY,
+    base_url=f"{ZEP_API_URL}/api/v2",
+)
+
+# Load
+loader = WebBaseLoader(SOURCE)
+data = loader.load()
+
+print(f"Loaded: {len(data)} documents")
+
+# Split
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=400, chunk_overlap=200)
+all_splits = text_splitter.split_documents(data)
+
+print(f"Adding {len(all_splits)} documents to {ZEP_COLLECTION_NAME}...")
+
+# Add to vectorDB
+vectorstore = ZepVectorStore.from_documents(
+    documents=all_splits,
+    collection_name=ZEP_COLLECTION_NAME,
+    zep_client=zep,
+)
+
+print(f"Added {len(all_splits)} documents to {ZEP_COLLECTION_NAME}...")
