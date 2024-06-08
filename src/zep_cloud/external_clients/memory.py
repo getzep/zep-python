3a88ae848ef309b15c612c23d1cc3be0a94e9198
Daniel Chalef <131175+danielchalef@users.noreply.github.com>
2024-06-07 18:51:47 -0700

    wip

--- src/zep_cloud/external_clients/memory.py
+++ src/zep_cloud/external_clients/memory.py
@@ -1,16 +1,64 @@
+import datetime
+import json
+import typing
 from zep_cloud.core.client_wrapper import AsyncClientWrapper, SyncClientWrapper
-from zep_cloud.memory.client import MemoryClient as BaseMemoryClient, AsyncMemoryClient as AsyncBaseMemoryClient
+from zep_cloud.memory.client import (
+    MemoryClient as BaseMemoryClient,
+    AsyncMemoryClient as AsyncBaseMemoryClient,
+)
+
+from zep_cloud.extractor.models import ZepModel
 
 
 class MemoryClient(BaseMemoryClient):
     def __init__(self, *, client_wrapper: SyncClientWrapper):
-        super().__init__(
-            client_wrapper=client_wrapper
+        super().__init__(client_wrapper=client_wrapper)
+
+    def extract(
+        self,
+        session_id: str,
+        model: ZepModel,
+        current_date_time: typing.Optional[datetime.datetime] = None,
+        last_n: typing.Optional[int] = 4,
+        validate: typing.Optional[bool] = False,
+    ):
+        model_schema = json.dumps(model.model_json_schema())
+
+        result = self.extract_data(
+            session_id=session_id,
+            model_schema=model_schema,
+            validate=validate,
+            last_n=last_n,
+            current_date_time=current_date_time,
         )
+
+        return model.model_validate(result)
 
 
 class AsyncMemoryClient(AsyncBaseMemoryClient):
     def __init__(self, *, client_wrapper: AsyncClientWrapper):
-        super().__init__(
-            client_wrapper=client_wrapper
+        super().__init__(client_wrapper=client_wrapper)
+
+    async def extract(
+        self,
+        session_id: str,
+        model: ZepModel,
+        current_date_time: typing.Optional[datetime.datetime] = None,
+        last_n: typing.Optional[int] = 4,
+        validate: typing.Optional[bool] = False,
+    ):
+        model_schema = json.dumps(model.model_json_schema())
+
+        result = await self.extract_data(
+            session_id=session_id,
+            model_schema=model_schema,
+            validate=validate,
+            last_n=last_n,
+            current_date_time=current_date_time,
         )
+
+        result = {k: v for k, v in result.items() if v is not (None or "")}
+
+        print(result)
+
+        return model.model_validate(result)
---
+++ src/zep_cloud/extractor/__init__.py
@@ -0,0 +1,25 @@
+from zep_cloud.extractor.models import (
+    ZepModel,
+    ZepText,
+    ZepNumber,
+    ZepFloat,
+    ZepRegex,
+    ZepZipCode,
+    ZepDate,
+    ZepDateTime,
+    ZepEmail,
+    ZepPhoneNumber,
+)
+
+__all__ = [
+    "ZepModel",
+    "ZepText",
+    "ZepNumber",
+    "ZepFloat",
+    "ZepRegex",
+    "ZepZipCode",
+    "ZepDate",
+    "ZepDateTime",
+    "ZepEmail",
+    "ZepPhoneNumber",
+]
