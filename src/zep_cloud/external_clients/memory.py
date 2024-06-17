648f6104d14d1a088a9d2dd832fd5071c430ca14
Pavlo Paliychuk <paul@getzep.com>
2024-06-17 18:12:21 -0400

    Document extract method (#191)
    * Facts search (#189)
    
    * SDK regeneration
    
    * SDK regeneration
    
    * chore: Add search multiple sessions example
    
    * SDK regeneration
    
    * chore: Update search sessions example signature
    
    * SDK regeneration
    
    * SDK regeneration
    
    * implement sde; update ci to use python3.9 (#187)
    
    * wip
    
    * wip
    
    * ruff it
    
    * update ci
    
    * chore: Update Makefile with test coverage and linting commands
    
    * chore: Update Makefile with linting command and fix ruff check path
    
    * chore: Refactor memory client to use keyword arguments for optional parameters
    
    * chore: Add Makefile to .fernignore
    
    * Add extractor folder to fernignore
    
    Signed-off-by: Pavlo Paliychuk <paul@getzep.com>
    
    ---------
    
    Signed-off-by: Pavlo Paliychuk <paul@getzep.com>
    Co-authored-by: Pavlo Paliychuk <paul@getzep.com>
    
    * Version Bump
    
    Signed-off-by: Pavlo Paliychuk <paul@getzep.com>
    
    * SDK regeneration
    
    * SDK regeneration
    
    * chore: Update memory example to use new search interface
    
    ---------
    
    Signed-off-by: Pavlo Paliychuk <paul@getzep.com>
    Co-authored-by: fern-api <115122769+fern-api[bot]@users.noreply.github.com>
    Co-authored-by: paulpaliychuk <pavlo.paliychuk.ca@gmail.com>
    Co-authored-by: Daniel Chalef <131175+danielchalef@users.noreply.github.com>
    Co-authored-by: Pavlo Paliychuk <paul@getzep.com>
    
    * chore: Add code comments to extract methods
    
    * Update src/zep_cloud/external_clients/memory.py
    
    Co-authored-by: ellipsis-dev[bot] <65095814+ellipsis-dev[bot]@users.noreply.github.com>
    Signed-off-by: Daniel Chalef <131175+danielchalef@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Pavlo Paliychuk <paul@getzep.com>
    Signed-off-by: Daniel Chalef <131175+danielchalef@users.noreply.github.com>
    Co-authored-by: Travis Beauvais <tbeauvais@gmail.com>
    Co-authored-by: fern-api <115122769+fern-api[bot]@users.noreply.github.com>
    Co-authored-by: Daniel Chalef <131175+danielchalef@users.noreply.github.com>
    Co-authored-by: ellipsis-dev[bot] <65095814+ellipsis-dev[bot]@users.noreply.github.com>

--- src/zep_cloud/external_clients/memory.py
+++ src/zep_cloud/external_clients/memory.py
@@ -24,6 +24,47 @@ class MemoryClient(BaseMemoryClient):
         last_n: int = 4,
         validate: bool = False,
     ):
+        """Extracts structured data from a session based on a ZepModel schema.
+        This method retrieves data based on a given model and session details.
+        It then returns the extracted and validated data as an instance of the given ZepModel.
+
+        Parameters
+        ----------
+        session_id: str
+            Session ID.
+        model: ZepModel
+            An instance of a ZepModel subclass defining the expected data structure and field types.
+        current_date_time: typing.Optional[datetime.datetime]
+            Your current date and time in ISO 8601 format including timezone.
+            This is used for determining relative dates.
+        last_n: typing.Optional[int]
+            The number of messages in the chat history from which to extract data.
+        validate: typing.Optional[bool]
+            Validate that the extracted data is present in the dialog and correct per the field description.
+            Mitigates hallucination, but is slower and may result in false negatives.
+
+        Returns
+        -------
+        ZepModel: An instance of the provided ZepModel subclass populated with the
+            extracted and validated data.
+
+        Examples
+        --------
+        class CustomerInfo(ZepModel):
+            name: Optional[ZepText] = Field(description="Customer name", default=None)
+            email: Optional[ZepEmail] = Field(description="Customer email", default=None)
+            signup_date: Optional[ZepDate] = Field(description="Customer Sign up date", default=None)
+
+        client = AsyncMemoryClient(...)
+
+        customer_data = await client.memory.extract(
+            session_id="session123",
+            model=CustomerInfo(),
+            current_date_time=datetime.datetime.now(),  # Filter data up to now
+        )
+
+        print(customer_data.name)  # Access extracted and validated customer name
+        """
         model_schema = json.dumps(model.model_json_schema())
 
         result = self.extract_data(
@@ -51,6 +92,47 @@ class AsyncMemoryClient(AsyncBaseMemoryClient):
         last_n: int = 4,
         validate: bool = False,
     ):
+        """Extracts structured data from a session based on a ZepModel schema.
+       This method retrieves data based on a given model and session details.
+       It then returns the extracted and validated data as an instance of the given ZepModel.
+
+       Parameters
+       ----------
+       session_id: str
+           Session ID.
+       model: ZepModel
+           An instance of a ZepModel subclass defining the expected data structure and field types.
+       current_date_time: typing.Optional[datetime.datetime]
+           Your current date and time in ISO 8601 format including timezone.
+           This is used for determining relative dates.
+       last_n: typing.Optional[int]
+           The number of messages in the chat history from which to extract data.
+       validate: typing.Optional[bool]
+           Validate that the extracted data is present in the dialog and correct per the field description.
+           Mitigates hallucination, but is slower and may result in false negatives.
+
+       Returns
+       -------
+       ZepModel: An instance of the provided ZepModel subclass populated with the
+           extracted and validated data.
+
+       Examples
+       --------
+       class CustomerInfo(ZepModel):
+           name: Optional[ZepText] = Field(description="Customer name", default=None)
+           name: Optional[ZepEmail] = Field(description="Customer email", default=None)
+           signup_date: Optional[ZepDate] = Field(description="Customer Sign up date", default=None)
+
+       client = AsyncMemoryClient(...)
+
+       customer_data = await client.memory.extract(
+           session_id="session123",
+           model=CustomerInfo(),
+           current_date_time=datetime.datetime.now(),  # Filter data up to now
+       )
+
+       print(customer_data.name)  # Access extracted and validated customer name
+       """
         model_schema = json.dumps(model.model_json_schema())
 
         result = await self.extract_data(
