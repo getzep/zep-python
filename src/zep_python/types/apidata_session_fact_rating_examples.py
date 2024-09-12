9dfc6d4bfb3aff53c56ab74ef3c35407e0743a72
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-09-12 03:36:31 +0000

    SDK regeneration

--- src/zep_python/types/apidata_session_fact_rating_examples.py
+++ src/zep_python/types/apidata_session_fact_rating_examples.py
@@ -5,17 +5,12 @@ import typing
 
 from ..core.datetime_utils import serialize_datetime
 from ..core.pydantic_utilities import deep_union_pydantic_dicts, pydantic_v1
-from .apidata_fact import ApidataFact
-from .apidata_message import ApidataMessage
-from .apidata_summary import ApidataSummary
 
 
-class ApidataSessionSearchResult(pydantic_v1.BaseModel):
-    fact: typing.Optional[ApidataFact] = None
-    message: typing.Optional[ApidataMessage] = None
-    score: typing.Optional[float] = None
-    session_id: typing.Optional[str] = None
-    summary: typing.Optional[ApidataSummary] = None
+class ApidataSessionFactRatingExamples(pydantic_v1.BaseModel):
+    high: typing.Optional[str] = None
+    low: typing.Optional[str] = None
+    medium: typing.Optional[str] = None
 
     def json(self, **kwargs: typing.Any) -> str:
         kwargs_with_defaults: typing.Any = {"by_alias": True, "exclude_unset": True, **kwargs}
