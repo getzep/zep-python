cef27e9aa2c98fa6716199c0420078c8f32db551
Pavlo Paliychuk <paul@getzep.com>
2024-09-26 09:26:22 -0400

    Append api sufix to base url (#212)
    * SDK regeneration
    
    * SDK regeneration
    
    * SDK regeneration
    
    * SDK regeneration
    
    * SDK regeneration
    
    * SDK regeneration
    
    * Append api prefix (#211)
    
    * chore: rm chainlit example
    
    * chore: Add base url suffix in client constructor
    
    * fix: Assignment
    
    * chore: Version bump
    
    ---------
    
    Co-authored-by: fern-api <115122769+fern-api[bot]@users.noreply.github.com>

--- src/zep_python/client.py
+++ src/zep_python/client.py
@@ -1,32 +1,35 @@
-from .base_client import \
-    BaseClient, AsyncBaseClient
+from .base_client import BaseClient, AsyncBaseClient
 import typing
 import os
 import httpx
-from .environment import ZepEnvironment
 from .external_clients.memory import MemoryClient, AsyncMemoryClient
 from .external_clients.user import UserClient, AsyncUserClient
+
+api_suffix = "api/v2"
 
 
 class Zep(BaseClient):
     def __init__(
-            self,
-            *,
-            base_url: str = None,
-            api_key: typing.Optional[str] = os.getenv("ZEP_API_KEY"),
-            timeout: typing.Optional[float] = None,
-            follow_redirects: typing.Optional[bool] = None,
-            httpx_client: typing.Optional[httpx.Client] = None
+        self,
+        *,
+        base_url: str = None,
+        api_key: typing.Optional[str] = os.getenv("ZEP_API_KEY"),
+        timeout: typing.Optional[float] = None,
+        follow_redirects: typing.Optional[bool] = None,
+        httpx_client: typing.Optional[httpx.Client] = None,
     ):
+        api_url = ""
         env_api_url = os.getenv("ZEP_API_URL")
         if env_api_url:
-            base_url = f"{env_api_url}/api/v2"
+            api_url = f"{env_api_url}/{api_suffix}"
+        else:
+            api_url = f"${base_url}/{api_suffix}"
         super().__init__(
-            base_url=base_url,
+            base_url=api_url,
             api_key=api_key,
             timeout=timeout,
             follow_redirects=follow_redirects,
-            httpx_client=httpx_client
+            httpx_client=httpx_client,
         )
         self.memory = MemoryClient(client_wrapper=self._client_wrapper)
         self.user = UserClient(client_wrapper=self._client_wrapper)
@@ -34,23 +37,26 @@ class Zep(BaseClient):
 
 class AsyncZep(AsyncBaseClient):
     def __init__(
-            self,
-            *,
-            base_url: str = None,
-            api_key: typing.Optional[str] = os.getenv("ZEP_API_KEY"),
-            timeout: typing.Optional[float] = None,
-            follow_redirects: typing.Optional[bool] = None,
-            httpx_client: typing.Optional[httpx.AsyncClient] = None
+        self,
+        *,
+        base_url: str = None,
+        api_key: typing.Optional[str] = os.getenv("ZEP_API_KEY"),
+        timeout: typing.Optional[float] = None,
+        follow_redirects: typing.Optional[bool] = None,
+        httpx_client: typing.Optional[httpx.AsyncClient] = None,
     ):
+        api_url = ""
         env_api_url = os.getenv("ZEP_API_URL")
         if env_api_url:
-            base_url = f"{env_api_url}/api/v2"
+            api_url = f"{env_api_url}/{api_suffix}"
+        else:
+            api_url = f"${base_url}/{api_suffix}"
         super().__init__(
-            base_url=base_url,
+            base_url=api_url,
             api_key=api_key,
             timeout=timeout,
             follow_redirects=follow_redirects,
-            httpx_client=httpx_client
+            httpx_client=httpx_client,
         )
         self.memory = AsyncMemoryClient(client_wrapper=self._client_wrapper)
         self.user = AsyncUserClient(client_wrapper=self._client_wrapper)
